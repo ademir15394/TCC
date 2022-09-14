f0db841d89b5575c45742c97adbb78638634668c
ademir15394 <99704646+ademir15394@users.noreply.github.com>
2022-09-14 14:51:33 -0300

    Add files via upload

--- projeto tcc/app.js
+++ projeto tcc/app.js
@@ -52,3 +52,16 @@ var swiper = new Swiper(".home-slider", {
       },
     },
   });
+
+
+
+
+  document.querySelectorAll(".text-input").forEach((element) => {
+    element.addEventListener("blur", (event) => {
+      if (event.target.value != "") {
+        event.target.nextElementSibling.classList.add("filled");
+      } else {
+        event.target.nextElementSibling.classList.remove("filled");
+      }
+    });
+  });
